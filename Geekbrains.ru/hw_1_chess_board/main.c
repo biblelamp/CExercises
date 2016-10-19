b63f52a89aaa3ae47ccbf814b29949b382e20176
Sergey Iryupin <biblelamp@yandex.ru>
2016-10-19 13:09:16 +0300

    homework 1. chess board

---
+++ Geekbrains.ru/hw_1_chess_board/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x1,y1,x2,y2;
+    printf("Enter coordinates x1 y1 x2 y2 (1..8):\n");
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    if ((x1 + y1) % 2 == (x1 + y1) % 2)
+    {
+        printf("The cells have equal color");
+    }
+    else
+    {
+        printf("The cells have non equal color");
+    }
+
+    return 0;
+}
