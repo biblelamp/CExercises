f458caa6ee0972bc1c9d1333ff6c7bbd929f7f1b
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-14 17:22:09 +0300

    homework 8. stack

---
+++ Geekbrains.ru/hw_8_stack/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <malloc.h>
+#define T char
+#define zero '\0'
+
+struct TNode
+{
+    T value ; // data
+    struct TNode * next ; // a pointer to the next item in the list
+};
+
+typedef struct TNode Node;
+
+struct Stack
+{
+    Node * head;
+    int size;
+    int maxSize;
+};
+
+struct Stack Stack;
+
+void push(T value)
+{
+    if (Stack.size >= Stack.maxSize)
+    {
+        printf("Error stack size.\n");
+        return;
+    }
+    Node * tmp = (Node *) malloc(sizeof (Node));
+    tmp -> value = value;
+    tmp -> next = Stack.head;
+    Stack.head = tmp;
+    Stack.size++;
+}
+
+T pop () {
+    if (Stack.size == 0 )
+    {
+        printf("Stack is empty.\n");
+        return zero;
+    }
+    Node * next = NULL;
+    T value;
+    value = Stack.head -> value;
+    next = Stack.head;
+    Stack.head = Stack.head -> next;
+    free(next);
+    Stack.size--;
+    return value;
+}
+
+void printStack ()
+{
+    Node * current = Stack.head;
+    while (current != NULL)
+    {
+        printf("%c ", current -> value);
+        current = current -> next;
+    }
+}
+
+int main()
+{
+    Stack.maxSize = 100;
+    Stack.head = NULL;
+    push('a');
+    push('b');
+    push('c');
+    push('d');
+    push('e');
+    push('f');
+    printStack();
+    printf("\n");
+    printf("%c\n", pop());
+    printf("%c\n", pop());
+    printStack();
+    return 0;
+}
