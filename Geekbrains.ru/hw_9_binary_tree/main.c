676ef4a11108769b3369e368150c59cdeb127383
Sergey Iryupin <biblelamp@yandex.ru>
2016-11-17 11:45:38 +0300

    homework 9. binary tree added tree traversal function

--- Geekbrains.ru/hw_9_binary_tree/main.c
+++ Geekbrains.ru/hw_9_binary_tree/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <malloc.h>
+#define array_size(_a) (int)(sizeof _a / sizeof _a[0])
 
 typedef int T;
 
 typedef struct Node
 {
-   T data;
-   struct Node *left;
-   struct Node *right;
-   struct Node *parent;
+    T data;
+    struct Node *left;
+    struct Node *right;
+    struct Node *parent;
 } Node;
 
 void printTree(Node *root, const char *dir, int level)
@@ -33,7 +34,6 @@ Node* getFreeNode(T value, Node *parent)
 void insert(Node **head, int value)
 {
     Node *tmp = NULL;
-    Node *ins = NULL;
     if (*head == NULL)
     {
         *head = getFreeNode(value, NULL);
@@ -67,21 +67,59 @@ void insert(Node **head, int value)
                 tmp->left = getFreeNode(value, tmp);
                 return;
             }
-       }
+        }
         else
         {
-            exit(2);
+            break;
         }
     }
 }
 
-int main(int argc, char *argv[])
+void preOrderTravers(Node *root)
+{
+    if (root)
+    {
+        printf("%d ", root->data);
+        preOrderTravers(root->left);
+        preOrderTravers(root->right);
+    }
+}
+
+void inOrderTravers(Node *root)
+{
+    if (root)
+    {
+        inOrderTravers(root->left);
+        printf("%d ", root->data);
+        inOrderTravers(root->right);
+    }
+}
+
+void postOrderTravers(Node *root)
+{
+    if (root)
+    {
+        postOrderTravers(root->left);
+        postOrderTravers(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
 {
     Node *Tree=NULL;
-    insert(&Tree, 10);
-    insert(&Tree, 5);
-    insert(&Tree, 20);
-    insert(&Tree, 15);
+    int a[] = {10, 5, 20, 15, 30, 25};
+    for (int i = 0; i < array_size(a); i++)
+        insert(&Tree, a[i]);
     printTree(Tree, "root", 0);
+    printf("preOrder: ");
+    preOrderTravers(Tree);
+    printf("\n");
+    printf("inOrder: ");
+    inOrderTravers(Tree);
+    printf("\n");
+    printf("postOrder: ");
+    postOrderTravers(Tree);
+    printf("\n");
     return 0;
 }
